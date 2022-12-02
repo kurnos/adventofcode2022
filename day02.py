c2999c341efc0eede341940fe6aaf5ac08ef9e30
Johan Olsson <johan.olsson@juni.co>
2022-12-02 14:21:36 +0100

    make day2 nicer :)

--- day02.py
+++ day02.py
@@ -1,33 +1,15 @@
+x = lambda c: ord(c) % 23 - 19 # maps ABC and XYZ to 012
+def scoreA(line):
+    elf, me = x(line[0]), x(line[2])
+    return 1 + me + ((me - elf + 1) % 3)*3
+
+def scoreB(line):
+    elf, result = x(line[0]), x(line[2])
+    return 1 + (2 + elf + result) % 3 + result * 3
+
 def solve(data):
-    scoresA = {
-        "A X": 1 + 3,
-        "B X": 1 + 0,
-        "C X": 1 + 6,
-
-        "A Y": 2 + 6,
-        "B Y": 2 + 3,
-        "C Y": 2 + 0,
-
-        "A Z": 3 + 0,
-        "B Z": 3 + 6,
-        "C Z": 3 + 3,
-    }
-
-    scoresB = {
-        "A X": 3 + 0,
-        "A Y": 1 + 3,
-        "A Z": 2 + 6,
-
-        "B X": 1 + 0,
-        "B Y": 2 + 3,
-        "B Z": 3 + 6,
-
-        "C X": 2 + 0,
-        "C Y": 3 + 3,
-        "C Z": 1 + 6,
-    }
-    print("a", sum(scoresA[d] for d in data.split("\n")))
-    print("b", sum(scoresB[d] for d in data.split("\n")))
+    print("a", sum(scoreA(d) for d in data.split("\n")))
+    print("b", sum(scoreB(d) for d in data.split("\n")))
 
 if __name__ == '__main__':
     solve(open("data/day02").read())
