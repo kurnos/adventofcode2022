3b5b414588ab3cdbf741f1b9045546ec063cef17
Johan Olsson <kurnos@gmail.com>
2022-12-03 09:02:49 +0100

    pep8 formatting

--- day01.py
+++ day01.py
@@ -1,8 +1,9 @@
-def solve():
-    elves = [sum(int(f) for f in s.split()) for s in open("data/day01").read().split("\n\n")]
+def solve(data):
+    elves = [sum(int(f) for f in s.split())
+             for s in data.split("\n\n")]
     print("a", max(elves))
     print("b", sum(sorted(elves)[-3:]))
 
 
 if __name__ == "__main__":
-    solve()
+    solve(open("data/day01").read())
--- day02.py
+++ day02.py
@@ -1,15 +1,20 @@
-x = lambda c: ord(c) % 23 - 19 # maps ABC and XYZ to 012
+def x(c): return ord(c) % 23 - 19  # maps ABC and XYZ to 012
+
+
 def scoreA(line):
     elf, me = x(line[0]), x(line[2])
     return 1 + me + ((me - elf + 1) % 3)*3
+
 
 def scoreB(line):
     elf, result = x(line[0]), x(line[2])
     return 1 + (2 + elf + result) % 3 + result * 3
 
+
 def solve(data):
     print("a", sum(scoreA(d) for d in data.split("\n")))
     print("b", sum(scoreB(d) for d in data.split("\n")))
+
 
 if __name__ == '__main__':
     solve(open("data/day02").read())
